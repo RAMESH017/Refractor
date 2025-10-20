a45c71e8b6f9f5a7f569b4b23cd2ed1ee46d3aca
RAMESH017 <abdramesh007@gmail.com>
2025-10-20 14:30:33 +0530

    barking

--- basic.java
+++ basic.java
@@ -1,9 +1,17 @@
-public class Dog {
-    public void barkBark() {  // renamed method
-        System.out.println("Woof!");
-    }
-
-    public void sit() {
-        System.out.println("Sitting");
-    }
+class Animal {  
+    void sound() { System.out.println("Animal makes sound"); }  
+}  
+class Dog extends Animal {  
+    void sound() { System.out.println("Dog barks"); }  
+}  
+class Cat extends Animal {  
+    void sound() { System.out.println("Cat meows"); }  
+}  
+public class basic {  
+    public static void main(String[] args) {  
+        Animal a1 = new Dog();  
+        Animal a2 = new Cat();  
+        a1.sound();  
+        a2.sound();  
+    }  
 }
